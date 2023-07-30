ac7c87aa64c4a80a3bfa1b3f1d2f077648b84124
Yakiyo <yakiyo1206@gmail.com>
2023-07-30 17:09:00 +0000

    fix: dont filter anything

--- script/transform.go
+++ script/transform.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	"regexp"
 	"strings"
 )
-
-// Simple regex to check string contains at least the main vers that is x.y.z
-var re = regexp.MustCompile(`([1-9]+)\.([1-9]+)\.([1-9]+)`)
 
 // The values received from the api are in the format of
 // `channels/{{channel}}/release/{{version}}/`. This func
