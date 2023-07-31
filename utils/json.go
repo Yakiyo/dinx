06fa38a0ad11a23b33ef72df67af3d7f1e6783d9
Yakiyo <yakiyo1206@gmail.com>
2023-07-31 13:50:38 +0000

    feat: simple json reading util func

---
+++ utils/json.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"os"
+
+	json "github.com/json-iterator/go"
+)
+
+// Read json from file
+func ReadVersions(path string) (map[string]string, error) {
+	f, err := os.ReadFile(path)
+	m := map[string]string{}
+
+	if err != nil {
+		return m, err
+	}
+	err = json.Unmarshal(f, &m)
+	if err != nil {
+		return m, err
+	}
+	return m, nil
+}
